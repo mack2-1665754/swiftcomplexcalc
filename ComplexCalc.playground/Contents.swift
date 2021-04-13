b06dbaaf7e4175784e62ecb34180374f235b9f36
mackenziehutchison <mack2@uw.edu>
2021-04-13 14:13:48 -0700

    finished

---
+++ ComplexCalc.playground/Contents.swift
@@ -0,0 +1,159 @@
+print("Welcome back to the UW Calculator")
+
+// Your job is to fill out Calculator so all the expressions
+// below both compile and return "true"
+class Calculator {
+    func add(lhs: Int, rhs: Int) -> Int {
+        var result = Int()
+        result = lhs + rhs
+        return result
+    }
+    
+    func add(_ args: [Int]) -> Int {
+        var sum = Int()
+        for i in 0...args.count - 1 {
+            sum += args[i]
+        }
+        return sum
+    }
+    
+    func add(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
+        let lResult = lhs.0 + rhs.0
+        let rResult = lhs.1 + rhs.1
+        return (lResult, rResult)
+    }
+    
+    func add(lhs: [String: Int], rhs: [String: Int]) -> [String: Int] {
+        let resultX = lhs["x"]! + rhs["x"]!
+        let resultY = lhs["y"]! + rhs["y"]!
+        return ["x": resultX, "y": resultY]
+    }
+    
+    func subtract(lhs: Int, rhs: Int) -> Int {
+        var result = Int()
+        result = lhs - rhs
+        return result
+    }
+    
+    func subtract(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
+        let lResult = lhs.0 - rhs.0
+        let rResult = lhs.1 - rhs.1
+        return (lResult, rResult)
+    }
+    
+    func subtract(lhs: [String: Int], rhs: [String: Int]) -> [String: Int] {
+        let resultX = lhs["x"]! - rhs["x"]!
+        let resultY = lhs["y"]! - rhs["y"]!
+        return ["x": resultX, "y": resultY]
+    }
+    
+    func multiply(lhs: Int, rhs: Int) -> Int {
+        var result = Int()
+        result = lhs * rhs
+        return result
+    }
+    
+    func multiply(_ args: [Int]) -> Int {
+        var result = 1
+        for i in 0...args.count - 1 {
+            result *= args[i]
+        }
+        return result
+    }
+    
+    func divide(lhs: Int, rhs: Int) -> Int {
+        var result = Int()
+        result = lhs / rhs
+        return result
+    }
+    
+    func avg(_ args: [Int]) -> Int {
+        var sum = 0
+        let count = args.count
+        for i in 0...args.count - 1 {
+            sum += args[i]
+        }
+        return sum / count
+    }
+    
+    func count(_ args: [Int]) -> Int{
+        if (args.count < 1) {
+            return 0
+        }
+        return args[args.count - 1]
+    }
+    
+    func mathOp(lhs : Int, rhs : Int, op : (Int, Int) -> Int) -> Int {
+        return op(lhs, rhs)
+    }
+    
+    func mathOp(args: [Int], beg: Int, op : (Int, Int) -> Int) -> Int {
+        var currentVal = op(beg, args[0])
+        for i in 1...args.count - 1{
+            currentVal = op(currentVal, args[i])
+        }
+        print(currentVal)
+        return currentVal
+    }
+}
+
+
+let calc = Calculator()  // Don't change this declaration name; it's used in all the tests below
+
+// ====> Add your own tests here if you wish <====
+
+// I wrote separate test cases for anonymous function calls, as the right side does not show the boolean value but rather the number of times the anonymous function is called. This is to show that the mathOp outputs are correct.
+
+var res = calc.mathOp(lhs: 5, rhs: 5, op: { (lhs: Int, rhs: Int) -> Int in (lhs + rhs) + (lhs * rhs) }) == 35
+res
+// This style is one way of writing an anonymous function
+res = calc.mathOp(lhs: 10, rhs: -5, op: { ($0 + $1) + ($0 - $1) }) == 20
+    // This is the second, more terse, style; either works
+res
+res = calc.mathOp(args: [1, 2, 3], beg: 0, op: { $0 + $1 }) == 6
+res
+res = calc.mathOp(args: [1, 2, 3, 4, 5], beg: 0, op: { $0 + $1 }) == 15
+res
+    // this is (((((0 op 1) op 2) op 3) op 4) op 5)
+res = calc.mathOp(args: [1, 1, 1, 1, 1], beg: 1, op: { $0 * $1 }) == 1
+res
+    // this is (((((1 op 1) op 1) op 1) op 1) op 1)
+
+// ====> Do not modify code in this section <====
+calc.add(lhs: 2, rhs: 2) == 4
+calc.subtract(lhs: 2, rhs: 2) == 0
+calc.multiply(lhs: 2, rhs: 2) == 4
+calc.divide(lhs: 2, rhs: 2) == 1
+
+calc.mathOp(lhs: 5, rhs: 5, op: { (lhs: Int, rhs: Int) -> Int in (lhs + rhs) + (lhs * rhs) }) == 35
+    // This style is one way of writing an anonymous function
+calc.mathOp(lhs: 10, rhs: -5, op: { ($0 + $1) + ($0 - $1) }) == 20
+    // This is the second, more terse, style; either works
+calc.add([1, 2, 3, 4, 5]) == 15
+calc.multiply([1, 2, 3, 4, 5]) == 120
+calc.count([1, 2, 3, 4, 5, 6, 7, 8]) == 8
+calc.count([]) == 0
+calc.avg([2, 2, 2, 2, 2, 2]) == 2
+calc.avg([1, 2, 3, 4, 5]) == 3
+calc.avg([1]) == 1
+
+calc.mathOp(args: [1, 2, 3], beg: 0, op: { $0 + $1 }) == 6
+    // this is (((0 op 1) op 2) op 3)
+calc.mathOp(args: [1, 2, 3, 4, 5], beg: 0, op: { $0 + $1 }) == 15
+    // this is (((((0 op 1) op 2) op 3) op 4) op 5)
+calc.mathOp(args: [1, 1, 1, 1, 1], beg: 1, op: { $0 * $1 }) == 1
+    // this is (((((1 op 1) op 1) op 1) op 1) op 1)
+let p1 = (5, 5)
+let p2 = (12, -27)
+let p3 = (-4, 4)
+let p4 = (0, 0)
+calc.add(lhs: p1, rhs: p2) == (17, -22)
+calc.subtract(lhs: p1, rhs: p2) == (-7, 32)
+calc.add(lhs: p4, rhs: p4) == (0, 0)
+calc.add(lhs: p3, rhs: p4) == (-4, 4)
+
+let pd1 = ["x": 5, "y": 5]
+let pd2 = ["x": -4, "y": 4]
+calc.add(lhs: pd1, rhs: pd2) == ["x": 1, "y": 9]
+calc.subtract(lhs: pd1, rhs: pd2) == ["x": 9, "y": 1]
+
